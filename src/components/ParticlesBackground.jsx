4e504a1b0bf687ddb5560c1760e49b74101037a1
ArkalKeirosRas <recargamarcas6@gmail.com>
2023-04-28 02:05:59 -0500

    gh-pages added

---
+++ src/components/ParticlesBackground.jsx
@@ -0,0 +1,21 @@
+import React, { useCallback } from 'react'
+import Particles from 'react-tsparticles'
+import { loadFull } from 'tsparticles'
+import particlesConfig from './config/particles-config'
+
+const ParticlesBackground = () => {
+    const particlesInit = useCallback((engine) => {
+        loadFull(engine)
+    },[] )
+  return (
+    <div>
+        <Particles 
+         id = "tsparticles"
+         options = {particlesConfig}
+        init = {particlesInit}
+        />
+    </div>
+  )
+}
+
+export default ParticlesBackground
