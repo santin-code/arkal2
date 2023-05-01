758b73f30b9f199970f7bfecfa0d15de1edca597
ArkalKeirosRas <recargamarcas6@gmail.com>
2023-04-30 20:51:31 -0500

    <3

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import './App.css';
-import ParticlesBackground from './components/ParticlesBackground';
-import FreeFire from './components/freeFire/FreeFire';
-import LogoTitle from './components/logoTitle/LogoTitle';
+import "./App.css";
+import ParticlesBackground from "./components/ParticlesBackground";
+import FreeFire from "./components/freeFire/FreeFire";
+import LogoTitle from "./components/logoTitle/LogoTitle";
 
 function App() {
   return (
     <div className="App">
       <ParticlesBackground />
-      <LogoTitle />
-      <FreeFire />
-      {/* <a href="https://www.flaticon.es/iconos-gratis/astronauta" title="astronauta iconos">Astronauta iconos creados por wanicon - Flaticon</a> */}
+        <LogoTitle />
+        <FreeFire />
+        {/* <a href="https://www.flaticon.es/iconos-gratis/astronauta" title="astronauta iconos">Astronauta iconos creados por wanicon - Flaticon</a> */}
     </div>
   );
 }
--- src/components/freeFire/FreeFire.jsx
+++ src/components/freeFire/FreeFire.jsx
@@ -1,24 +1,58 @@
 import React from "react";
 import "./free_fire.css";
-import {GiDiamondHard} from 'react-icons/gi'
+import { GiDiamondHard } from "react-icons/gi";
 
 const FreeFire = () => {
   return (
-    <div >
-      <h1 className="free_fire_title">FREE FIRE</h1>
+      <div className="title_subtitle_container">
+      <img
+        className="free_fire_title"
+        src="https://fontmeme.com/permalink/230430/73495cded1cca4dfe37debaab0605df3.png"
+        alt="title free fire"
+      />
       <div className="free_fire_container">
-        <img
-          className="free_fire_gif"
-          src="https://i.giphy.com/media/Bk1etlqzsxWNSi0HFn/giphy.webp"
-          alt="free_fire"
-        />
+          <img
+            className="free_fire_gif"
+            src="https://i.giphy.com/media/Bk1etlqzsxWNSi0HFn/giphy.webp"
+            alt="free_fire"
+          />
         <div className="advertising">
           <GiDiamondHard className="icon_diamond" />
-          <div className="diamond_description">110 Diamantes: <div className="price_diamond"> MXN $19 <span className="asterisk"> *</span> </div> </div>
-          <div className="diamond_description">341 Diamantes: <div className="price_diamond"> MXN $59 <span className="asterisk"> *</span> </div> </div>
-          <div className="diamond_description">572 Diamantes: <div className="price_diamond"> MXN $99 <span className="asterisk"> *</span> </div> </div>
-          <div className="diamond_description">1,166 Diamantes: <div className="price_diamond"> MXN $199 <span className="asterisk"> *</span> </div> </div>
-          <div className="diamond_description">2,398 Diamantes: <div className="price_diamond"> MXN $379 <span className="asterisk"> *</span> </div> </div>
+          <div className="diamond_description">
+            110 Diamantes:{" "}
+            <div className="price_diamond">
+              {" "}
+              MXN $19 <span className="asterisk"> *</span>{" "}
+            </div>{" "}
+          </div>
+          <div className="diamond_description">
+            341 Diamantes:{" "}
+            <div className="price_diamond">
+              {" "}
+              MXN $59 <span className="asterisk"> *</span>{" "}
+            </div>{" "}
+          </div>
+          <div className="diamond_description">
+            572 Diamantes:{" "}
+            <div className="price_diamond">
+              {" "}
+              MXN $99 <span className="asterisk"> *</span>{" "}
+            </div>{" "}
+          </div>
+          <div className="diamond_description">
+            1,166 Diamantes:{" "}
+            <div className="price_diamond">
+              {" "}
+              MXN $199 <span className="asterisk"> *</span>{" "}
+            </div>{" "}
+          </div>
+          <div className="diamond_description">
+            2,398 Diamantes:{" "}
+            <div className="price_diamond">
+              {" "}
+              MXN $379 <span className="asterisk"> *</span>{" "}
+            </div>{" "}
+          </div>
         </div>
       </div>
     </div>
